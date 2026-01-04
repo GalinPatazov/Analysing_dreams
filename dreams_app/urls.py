dc0a50856edcd589c2f62ff19b068b4f59b71eb3
GalinPatazov <galin.patazov@gmail.com>
2026-01-04 23:18:25 +0200

    Day 11: Django project, dreams system

--- dreams_app/urls.py
+++ dreams_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import DreamCreateView, DreamListView
+from .views import DreamCreateView, DreamListView, dream_detail
+
+
+app_name = 'dreams_app'
 
 urlpatterns = [
     path('new/', DreamCreateView.as_view(), name='new_dream'),
     path('', DreamListView.as_view(), name='my_dreams'),
+    path('<int:pk>/', dream_detail, name='detail'),
 
 ]
